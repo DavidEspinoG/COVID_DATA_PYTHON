5bdac85a7c993ed82c330a556270ca6ce71e899a
David Espino <davidespinog@gmail.com>
2022-06-09 09:30:50 -0500

    Funcion busca_estados()

--- series.py
+++ series.py
@@ -1,8 +1,15 @@
 import scripts as scr
+string_matriz = scr.leer_archivo()
+int_matriz = scr.matriz_a_entero(string_matriz)
+fechas = string_matriz[0]
+def busca_estados(estado):
+    resultado = None
+    for e in int_matriz: 
+            if estado in e:
+                resultado = e
+    return resultado
 def main():
-    string_matriz = scr.leer_archivo()
-    int_matriz = scr.matriz_a_entero(string_matriz)
-    fechas = string_matriz[0]
-    print(int_matriz)
+    print(busca_estados('YUCATAN'))
+    
 if __name__ == '__main__':
     main() 
