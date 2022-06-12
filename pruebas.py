67459ccfcf52ce690a5830602a14388d6e2c99f7
David Espino <davidespinog@gmail.com>
2022-06-11 20:15:49 -0500

    Funcionalidad terminada

--- pruebas.py
+++ pruebas.py
@@ -1,10 +1,1 @@
-from matplotlib import pyplot as plt
-fechas = ['04-2020', '05-2020', '06-2020', '07-2020']
-casos = [1,5,3,4]
 
-fig, ax = plt.subplots(figsize=(15,7))
-ax.plot(fechas, casos)
-ax.grid(True)
-ax.set_title('Prueba')
-plt.xticks(fechas, rotation= 80)
-plt.show()
--- scripts.py
+++ scripts.py
@@ -1,5 +1,7 @@
 import os
 import series as ser
+import porcentaje as por
+import maximos as maxi
 RUTA_MAIN = os.path.dirname(__file__)
 RUTA = RUTA_MAIN + '/data/data.csv'
 def leer_archivo():
@@ -41,9 +43,20 @@ def matriz_a_entero(matriz):
     return matriz_int
 INT_MATRIZ = matriz_a_entero(STRING_MATRIZ)
 def dia_con_mas_casos():
-    print('Día con más casos')
+    maximos = maxi.regresa_max_contagios()
+    indices = maxi.index_max_contagios(maximos)
+    estados = regresa_estados(INT_MATRIZ)
+    fechas = maxi.fecha_max_contagio(indices)
+    matriz_final = maxi.regresa_matriz_maximos(estados, fechas, maximos)
+    columnas = ['Estado', 'Fecha', 'Máximo']
+    maxi.muestra_tabla_max_contagios(matriz_final, columnas)
+    maxi.grafica_maximos_estado(estados[:-1], maximos[:-1])
 def porcentaje_casos():
-    print('Porcentaje de casos')
+    datos_tabla_porcentajes = por.total_casos_estados(INT_MATRIZ)
+    por.datos_tabla(datos_tabla_porcentajes)
+
+    datos1, datos2 = por.datos_grafica_barras(datos_tabla_porcentajes)
+    por.grafica2(datos1,datos2)
 def series_tiempo():
     estado_user_input = input('''
         ==================================================================================
